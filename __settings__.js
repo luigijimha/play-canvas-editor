3e9e491348fd136140cc7ba4fad7f87b22321ef9
Luis Mario J. H. <mario.jimenez@anahuac.mx>
2024-09-24 00:48:04 -0600

    initial commit

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2076946.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 196998137, 196998138, 196998161, 196998141, 196998142, 196998143, 196998144, 196998145, 196998147, 196998148, 196998149, 196998150, 196998152, 196998153, 196998228 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/196998158/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/196998159/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/196998157/1/ammo.js', 'preload' : true},
+];
